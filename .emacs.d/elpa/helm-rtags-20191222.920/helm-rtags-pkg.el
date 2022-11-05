a721271b994cb79828bce2ad105e2c5fc90b409d
Romain Lacroix <not.an.email@email.email>
2022-11-05 22:49:44 +0100

    commiting the packages

---
+++ .emacs.d/elpa/helm-rtags-20191222.920/helm-rtags-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from helm-rtags-20191222.920/helm-rtags.el  -*- no-byte-compile: t -*-
+(define-package "helm-rtags" "20191222.920" "A front-end for rtags" '((helm "2.0") (rtags "2.10")) :commit "595055b5316a7c92ba1d638f324f98842a0f41a5" :authors '(("Jan Erik Hanssen" . "jhanssen@gmail.com") ("Anders Bakken" . "agbakken@gmail.com")) :maintainer '("Jan Erik Hanssen" . "jhanssen@gmail.com") :url "https://github.com/Andersbakken/rtags")
