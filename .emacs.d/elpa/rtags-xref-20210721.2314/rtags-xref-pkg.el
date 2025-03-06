5e75ded6bcaae51c49b0cfa29780b2502b93bbb9
Neptune Magic Sauce <37757244+NeptuneMagicSauce@users.noreply.github.com>
2025-03-06 18:26:03 +0100

    emacs package cleanup: remove unused, upgrade some

--- .emacs.d/elpa/rtags-xref-20210721.2314/rtags-xref-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from rtags-xref-20210721.2314/rtags-xref.el  -*- no-byte-compile: t -*-
-(define-package "rtags-xref" "20210721.2314" "RTags backend for xref.el" '((emacs "25.1") (rtags "2.37")) :commit "1f938a71106489e587c806181cdf2a0018a0cd41" :authors '(("Jörg Walter")) :maintainer '("RTags Team") :url "https://github.com/Andersbakken/rtags")
